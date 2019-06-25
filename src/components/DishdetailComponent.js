5e89eb627576cf7d4089f8c78c49c4fb6e2f1652
Eric Syme <spotlightphotoseattle@gmail.com>
2019-06-25 11:05:49 -0700

    redux thunk pt1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, Modal, ModalHeader, ModalBody, CardTitle, Row, Breadcrumb, BreadcrumbItem, Button, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
-
+import { Loading } from './LoadingComponent';
 
 
 function RenderDish({dish}){
@@ -120,8 +119,25 @@ export class CommentForm extends Component {
 }
 
 const DishDetail = (props) => {
-
-    if (props.dish != null){
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null){
         return (
             <div className="container">
                 <div className="row">
